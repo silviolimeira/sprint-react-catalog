6fcbcda96db392ee04929c37c5ffa2c51b23a6aa
Silvio Limeira <sicemal@gmail.com>
2022-05-24 18:15:39 -0300

    fix 1440px layout

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,7 +1,33 @@
+import { Button, Card } from "react-bootstrap";
+
+import "./styles.css";
+import { ReactComponent as MainImage } from "assets/images/main-image.svg";
+
 function Home() {
   return (
     <>
-      <h4>Home</h4>
+      <Card className="text-center">
+        <Card.Body className="page-home">
+          <div className="page-home-main-image">
+            <MainImage />
+          </div>
+          <div className="page-home-content">
+            <Card.Title>Conheça o melhor catálogo de produtos</Card.Title>
+            <Card.Text>
+              Ajudaremos vocÊ a encontrar os melhores produtos disponíveis no
+              mercado
+            </Card.Text>
+            <div className="page-home-botao">
+              <Button variant="primary text-white page-home-botao-b1">
+                &gt;
+              </Button>
+              <Button variant="primary text-white page-home-botao-b2">
+                INICIE AGORA A SUA BUSCA
+              </Button>
+            </div>
+          </div>
+        </Card.Body>
+      </Card>
     </>
   );
 }
