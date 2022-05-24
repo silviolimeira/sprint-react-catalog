34e2ebede0690adad267041096f50abe407c0dc0
Silvio Limeira <sicemal@gmail.com>
2022-05-24 08:09:52 -0300

    Example lazy loading

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarC from "./components/Navbar";
 
 import "assets/styles/scss/custom.scss";
+import Home from "pages/Home";
+/*const Home = lazy(() => import("pages/Home"));*/
+const HomeLazy = lazy(() => import("pages/Home"));
 
 function App() {
-  return <NavbarC />;
+  return (
+    <div className="App">
+      <NavbarC />
+      <Router>
+        <Suspense fallback={<div>Carregando...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="home" element={<HomeLazy />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- frontweb/src/index.tsx
+++ frontweb/src/index.tsx
@@ -4,21 +4,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Home from "pages/Home";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="home" element={<Home />} />
-    </Routes>
-  </BrowserRouter>
-);
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
